665759b5df2a13c12144d3d012109d5ac7ce2a5f
alvarogo1998 <114082652+alvarogo1998@users.noreply.github.com>
2022-09-28 16:57:31 +0200

    implement textview and imageview

---
+++ app/src/main/java/com/agalobr/pmdm/ut01/ex02/presentation/Ut01ex02Activity.kt
@@ -0,0 +1,11 @@
+package com.agalobr.pmdm.ut01.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01ex02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex02)
+    }
+}

9ec8271781d4e7e62d4ee3f479d10f5aebe070a0
alvarogo1998 <114082652+alvarogo1998@users.noreply.github.com>
2022-09-30 20:11:59 +0200

    implement imageview

--- app/src/main/java/com/agalobr/pmdm/ut01/ex02/presentation/Ut01ex02b.kt
+++ app/src/main/java/com/agalobr/pmdm/ut01/ex02/presentation/Ut01ex02b.kt
@@ -2,11 +2,10 @@ package com.agalobr.pmdm.ut01.ex02.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.agalobr.pmdm.R
 
-class Ut01ex02Activity : AppCompatActivity() {
+class Ut01ex02b : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01ex02)
+        setContentView(R.layout.activity_ut01ex02b)
     }
 }

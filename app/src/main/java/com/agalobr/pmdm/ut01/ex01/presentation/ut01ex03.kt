0a6e3855dae68e4bcee8f440f761746f0dfc8bb6
alvarogo1998 <114082652+alvarogo1998@users.noreply.github.com>
2022-10-07 16:21:14 +0200

    start exercise RelativeLayout

---
+++ app/src/main/java/com/agalobr/pmdm/ut01/ex01/presentation/ut01ex03.kt
@@ -0,0 +1,11 @@
+package com.agalobr.pmdm.ut01.ex01.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ut01ex03 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}

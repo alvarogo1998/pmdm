e033049e8c3132ad1c2a8249d4d574b0b6eabab7
alvarogo1998 <114082652+alvarogo1998@users.noreply.github.com>
2022-10-07 17:06:12 +0200

    start exercise Ut01Ex04

---
+++ app/src/main/java/com/agalobr/pmdm/ut01/ex01/presentation/Ut01ex04Activity.kt
@@ -0,0 +1,11 @@
+package com.agalobr.pmdm.ut01.ex01.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex04)
+    }
+}

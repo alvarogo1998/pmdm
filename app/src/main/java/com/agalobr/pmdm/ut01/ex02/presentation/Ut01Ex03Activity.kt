9669ec8c3cb38889027d2ed569df928e36ad9dee
alvarogo1998 <rovala18@hotmail.com>
2022-10-07 02:22:12 +0200

    End Activity Ut01Ex05

---
+++ app/src/main/java/com/agalobr/pmdm/ut01/ex02/presentation/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.agalobr.pmdm.ut01.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.agalobr.pmdm.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}
